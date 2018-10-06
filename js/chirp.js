b854b1ba1d81bdcfcc62706788fb8147be5af678
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2018-10-06 10:03:12 -1000

    first commit

--- js/chirp.js
+++ js/chirp.js
@@ -1,7 +1,15 @@
-function chirp(n){
-  // FIX ME
+function chirp (n) {
+  if (n === 0) {
+  return "";
+  }
+  if(n>=0){
+   return ('chirp '+ chirp(--n))
+  }
+    
 }
 
-$(document).ready(function(){
+
+$(document).ready(function () {
   $("#result").html(chirp(3));
-});
+  });
+
